4cbb52541a80d855c9a832296bb9309960e813a1
Marcos A Rosas <contactme@marcosarosas.dev>
2022-09-21 16:22:59 -0500

    Components separated

--- App.tsx
+++ App.tsx
@@ -1,48 +1,31 @@
-import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 
+import { Goal } from './src/types/interfaces';
+import GoalInput from './components/GoalInput';
+import GoalItem from './components/GoalItem';
+import { uniqueKey } from './src/utils/idGen';
 import { useState } from 'react';
 
 export default function App() {
-    const [enteredGoalText, setEnteredGoalText] = useState('');
     const [courseGoals, setCourseGoals] = useState<Goal[]>([]);
-    type Goal = {
-        id: string;
-        value: string;
-    };
 
-    const goalInputHandler = (enteredText: string) => {
-        setEnteredGoalText(enteredText);
-    };
-
-    const uniqueKey = () => {
-        return Math.random().toString();
-    };
-
-    const addGoalHandler = () => {
+    const addGoalHandler = (enteredGoalText: string) => {
         let newGoal: Goal = {
             id: uniqueKey(),
             value: enteredGoalText,
         };
         setCourseGoals((currentGoals) => [...currentGoals, newGoal]);
     };
+    const deleteGoalHandler = (goalId: string) => {};
+
     return (
         <View style={styles.appContainer}>
-            <View style={styles.textContainer}>
-                <TextInput
-                    style={styles.inputText}
-                    placeholder='Enter your Goal 🥅'
-                    onChangeText={goalInputHandler}
-                    placeholderTextColor='white'
-                />
-                <Button title='Add Goal' onPress={addGoalHandler} />
-            </View>
+            <GoalInput onAddGoal={addGoalHandler} />
             <View style={styles.goalsContainer}>
                 <FlatList
                     data={Object.values(courseGoals)}
                     renderItem={(itemData) => (
-                        <View style={styles.goalItems}>
-                            <Text>{itemData.item.value}</Text>
-                        </View>
+                        <GoalItem text={itemData.item.value} onDeleteItem={deleteGoalHandler} />
                     )}
                 />
             </View>
@@ -57,37 +40,8 @@ const styles = StyleSheet.create({
         paddingHorizontal: 16,
         backgroundColor: '#330303',
     },
-    textContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 24,
-        borderBottomWidth: 1,
-        borderBottomColor: '#000000',
-    },
-    inputText: {
-        width: '70%',
-        borderWidth: 1,
-        borderColor: 'black',
-        padding: 8,
-        marginRight: 8,
-        borderRadius: 4,
-        color: '#ffffff',
-        backgroundColor: '#000000',
-    },
+
     goalsContainer: {
         flex: 4,
-    },
-    goalItems: {
-        padding: 8,
-        marginVertical: 8,
-        backgroundColor: '#161616',
-        borderColor: 'black',
-        borderWidth: 1,
-        borderRadius: 4,
-    },
-    goalText: {
-        color: 'white',
     },
 });
